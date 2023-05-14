e26ca571b82394dfcb9c010ef2295c8f0ff3b6ae
jedwed <83894732+jedwed@users.noreply.github.com>
2023-05-15 09:30:13 +1000

    Completed basic siderbar layout and added light/dark mode toggle button

---
+++ components/sidebar.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
+// import musicNoteIcon from '../public/music-note.svg';
+// import githubIcon from '../public/github.svg';
+// import linkedinIcon from '../public/linkedin.svg';
+
+import { GithubIcon, LinkedInIcon, MusicNoteIcon } from './icons';
+
+const navItems = [
+  { path: '/', text: 'Home' },
+  { path: '/skills', text: 'Skills' },
+  { path: '/projects', text: 'Projects' },
+  { path: '/resume', text: 'Resume' },
+];
+
+export default function Sidebar() {
+  const { theme, setTheme } = useTheme();
+  return (
+    <div className="h-full w-[15%] flex flex-col items-center text-center py-3 justify-center">
+      <div>
+        {/* TODO: Logo */}
+        <h1 className="font-bold text-2xl">Edward Qian</h1>
+      </div>
+      <div className="flex flex-col">
+        {navItems.map(({ path, text }) => (
+          <Link href={path} className="flex items-center my-1">
+            <MusicNoteIcon />
+            {text}
+          </Link>
+        ))}
+      </div>
+      <div className="flex gap-2">
+        <a href="https://github.com/jedwed">
+          <GithubIcon />
+        </a>
+        <a href="https://www.linkedin.com/in/edward-q-6001b5221/">
+          <LinkedInIcon />
+        </a>
+      </div>
+      <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+        Toggle mode
+      </button>
+    </div>
+  );
+}
