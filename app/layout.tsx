d517c43dc7930be304804d70d8ec6c5ed8d62725
jedwed <83894732+jedwed@users.noreply.github.com>
2023-05-22 21:32:44 +1000

    Added transition for theme toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>
-          <div className="flex flex-col md:flex-row md:overflow-y-hidden">
+          <div className="flex flex-col md:flex-row md:overflow-y-hidden transition duration-200 bg-white dark:bg-black">
             <Navbar />
             <div className="w-full flex justify-center h-screen md:overflow-y-auto">
               <main className="my-auto p-8 text-center items-center max-w-xl">
