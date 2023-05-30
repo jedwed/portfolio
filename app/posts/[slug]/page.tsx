d265656c46fce0411f6d6a2f42a9d8bc890dff2f
jedwed <83894732+jedwed@users.noreply.github.com>
2023-05-30 17:36:03 +1000

    Convert blog posts to MDX files

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,31 +1,36 @@
+import { notFound } from 'next/navigation';
 import { format, parseISO } from 'date-fns';
 import { allPosts } from 'contentlayer/generated';
+import { useMDXComponent } from 'next-contentlayer/hooks';
 
 export const generateStaticParams = async () =>
   allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
 
-export const generateMetadata = ({ params }: { params: { slug: string } }) => {
-  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
-  if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
-  return { title: post.title };
-};
+// export const generateMetadata = ({ params }: { params: { slug: string } }) => {
+//   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+//   if (!post) {
+//     throw new Error(`Post not found for slug: ${params.slug}`);
+//   }
+//   return { title: post.title };
+// };
 
 const PostLayout = ({ params }: { params: { slug: string } }) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
-  if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
+  if (!post) {
+    notFound();
+  }
+
+  const MDXContent = useMDXComponent(post.body.code);
 
   return (
-    <article className="mx-auto max-w-xl py-8">
-      <div className="mb-8 text-center">
-        <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
+    <article>
+      <div className="text-center">
+        <time dateTime={post.date}>
           {format(parseISO(post.date), 'LLLL d, yyyy')}
         </time>
-        <h1 className="text-3xl font-bold">{post.title}</h1>
+        <h1>{post.title}</h1>
       </div>
-      <div
-        className="[&>*]:mb-3 [&>*:last-child]:mb-0"
-        dangerouslySetInnerHTML={{ __html: post.body.html }}
-      />
+      <MDXContent />
     </article>
   );
 };
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,25 +1,19 @@
 import Link from 'next/link';
 import { compareDesc, format, parseISO } from 'date-fns';
 import { allPosts, Post } from 'contentlayer/generated';
+import { useMDXComponent } from 'next-contentlayer/hooks';
 
 function PostCard(post: Post) {
+  const MDXContent = useMDXComponent(post.body.code);
   return (
-    <div className="mb-8">
-      <h2 className="mb-1 text-xl">
-        <Link
-          href={post.url}
-          className="text-blue-700 hover:text-blue-900 dark:text-blue-400"
-        >
-          {post.title}
-        </Link>
+    <div>
+      <h2>
+        <Link href={post.url}>{post.title}</Link>
       </h2>
-      <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
+      <time dateTime={post.date}>
         {format(parseISO(post.date), 'LLLL d, yyyy')}
       </time>
-      <div
-        className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0"
-        dangerouslySetInnerHTML={{ __html: post.body.html }}
-      />
+      <MDXContent />
     </div>
   );
 }
@@ -30,13 +24,11 @@ export default function Blog() {
   );
 
   return (
-    <div className="mx-auto max-w-xl py-8">
-      <h1 className="mb-8 text-center text-2xl font-black">
-        Next.js + Contentlayer Example
-      </h1>
+    <section>
+      <h1>Blog Posts</h1>
       {posts.map((post, idx) => (
         <PostCard key={idx} {...post} />
       ))}
-    </div>
+    </section>
   );
 }
