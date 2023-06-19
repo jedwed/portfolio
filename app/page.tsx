c09fe53524013a0523765c4707f83ec024273e9d
jedwed <83894732+jedwed@users.noreply.github.com>
2023-06-19 19:43:38 +1000

    Removed margin on homepage heading

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <section className="prose dark:prose-invert text-center">
-      <h1 className="mb-4">
+      <h1>
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-800 to-purple-400">
           Welcome
         </span>
@@ -11,11 +11,10 @@ export default function Home() {
         My name is <b>Edward Qian</b>, and I&apos;m currently a university
         student studying and tutoring <b>Computer Science</b> at the{' '}
         <a href="https://www.unsw.edu.au" target="_blank">
-          <b>University of New South Wales</b>
+          University of New South Wales
         </a>
         .
       </p>
-      <br />
       <p>
         I am most passionate for <b>software development</b> as well as{' '}
         <b>music</b> 🎵. At any certain point in time I am most likely either
